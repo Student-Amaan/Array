08d38cc95f2d24276bd68c25ce5ef51c391b27a9
Amanullah <danishdarman06@gmail.com>
2025-10-04 03:58:47 +0000

    Array1

---
+++ Array/Array.js
@@ -0,0 +1,37 @@
+const array = [1,2,3,4,5]
+// console.log(array)
+
+
+const myHero = ["Aman","danish","darman"]
+// console.log(myHero)
+
+const newArray = new Array (1,2,3,4,5)
+// console.log(newArray[4])
+
+newArray.push(8,3)
+newArray.pop()
+// console.log(newArray)
+
+
+newArray.unshift(0)
+
+newArray.shift()
+
+// console.log(newArray)
+
+
+// console.log(newArray.includes(9))
+// console.log(newArray.indexOf(2))
+
+console.log("A ",newArray)
+
+const myn1 = newArray.slice(2,6)
+
+console.log(myn1)
+
+console.log("B ", newArray)
+
+
+const myn2 = newArray.splice(2,6)
+console.log("c ",newArray)
+
